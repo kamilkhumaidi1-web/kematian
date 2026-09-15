19bfca6fe6917eb1f24e55f8f55049461f50cd1f
kamilkhumaidi1-web <kamilkhumaidi1@sekolahindonesia.edu.my>
2026-09-15 08:08:44 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,13 @@
-self.addEventListener('fetch', function(event) {
-  // Kosongkan saja
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
